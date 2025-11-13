e16575011fc0db93dc292d2ae4890fcc468d9fd5
James Romanov <romanovjames1@gmail.com>
2025-11-13 23:21:38 +0500

    fix: register tsconfig paths

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import 'tsconfig-paths/register';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import 'tsconfig-paths/register';
 
 export async function createNestServer() {
   const app = await NestFactory.create(AppModule);
