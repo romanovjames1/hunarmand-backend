ce5e120cb6385f9dad63add7304229feb116ca4b
James Romanov <romanovjames1@gmail.com>
2025-11-13 23:29:13 +0500

    fix: register tsconfig paths

--- vercel.ts
+++ vercel.ts
@@ -1,14 +1,16 @@
-import { createNestServer } from './src/main';
+import { createNestServer } from './dist/src/main';
 import serverlessExpress from '@vendia/serverless-express';
 
+// Cache server between requests
 let cachedServer: any;
 
-export const handler = async (event: any, context: any) => {
+export default async function handler(req: any, res: any) {
   if (!cachedServer) {
     const app = await createNestServer();
     await app.init();
     const expressApp = app.getHttpAdapter().getInstance();
     cachedServer = serverlessExpress({ app: expressApp });
   }
-  return cachedServer(event, context);
-};
+
+  return cachedServer(req, res);
+}
