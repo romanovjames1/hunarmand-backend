adda44cbf307c078e24fbbdb0b443fdcc0051323
James Romanov <romanovjames1@gmail.com>
2025-11-13 23:16:31 +0500

    added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'tsconfig-paths/register';
 
 export async function createNestServer() {
   const app = await NestFactory.create(AppModule);
